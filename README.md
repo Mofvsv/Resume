99dbb482b6ab3fd86987c85613a623f400591702
Mustaf Eli <elimustaf@gmail.com>
2017-10-08 10:21:37 -0700

    update resume builder

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Resume
+# Resume with Javascript
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,34 @@
-$("#main").append("Mustaf Eli");
+var name = "Mustaf Eli";
+var role = "web Developer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
+
+var bio = {
+    "name": "Mustaf Eli",
+    "role": "Software Developer",
+    "contact info": {
+        "mobile": "555-555-55555",
+        "github": "@Mofvsv",
+        "location": "Seattle"
+    },
+    "Welcome message": "welcome welcome",
+    "skills": ["awesomeness", "delievering things", "cryogenic sleep", "saving the universe"],
+    "bio pic": "image/fry.jpg"
+};
+
+var work = {};
+work.position = "System Integrater";
+work.employer = " Udacity";
+work.years = 3;
+
+var education = {};
+education["name"] =  "Tennessee State Univ";
+education["years"] = "2010 - 2014";
+education["city"] = "Nashville";
+
+$("#main").append(work["position"]);
+$("#main").append(education.name);
