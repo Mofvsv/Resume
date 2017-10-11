b845231d2aa502ddc9ef8031aaac0eda5043ccfb
Mustaf Eli <elimustaf@gmail.com>
2017-10-10 22:16:36 -0700

    add jobs and projects to resumebuilder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,34 +1,56 @@
-var name = "Mustaf Eli";
-var role = "web Developer";
+// var name = "Mustaf Eli";
+// var role = "web Developer";
+// var formattedName = HTMLheaderName.replace("%data%", name);
+// $("#header").prepend(formattedName);
+// $("#header").append(formattedRole);
 
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-$("#header").prepend(formattedName);
-$("#header").append(formattedRole);
-
+var work = {
+    "jobs": [
+        {
+            "employer": "boeing",
+            "title": "system integrator",
+            "location": "Bellevue",
+            "dates": "2014 - 2015"
+        }
+    ]
+}
+var projects = {
+    "projects": [
+        {
+            "title": "",
+            "dates": ""
+        }
+    ]
+}
 var bio = {
-    "name": "Mustaf Eli",
-    "role": "Software Developer",
-    "contact info": {
-        "mobile": "555-555-55555",
-        "github": "@Mofvsv",
-        "location": "Seattle"
-    },
-    "Welcome message": "welcome welcome",
-    "skills": ["awesomeness", "delievering things", "cryogenic sleep", "saving the universe"],
-    "bio pic": "image/fry.jpg"
+    "bio": [
+        {
+            "name": "Mustaf Eli",
+            "role": "Software Developer",
+            "contact info": {
+                "mobile": "555-555-55555",
+                "github": "@Mofvsv",
+                "location": "Seattle"
+            },
+            "Welcome message": "welcome welcome",
+            "skills": ["awesomeness", "delievering things", "cryogenic sleep", "saving the universe"],
+            "bio pic": "image/fry.jpg"
+        }
+    ]
 };
 
-var work = {};
-work.position = "System Integrater";
-work.employer = " Udacity";
-work.years = 3;
-
-var education = {};
-education["name"] =  "Tennessee State Univ";
-education["years"] = "2010 - 2014";
-education["city"] = "Nashville";
-
-$("#main").append(work["position"]);
-$("#main").append(education.name);
+var education = {
+    "school": [
+        {
+            "name": "Tennessee State Univ",
+            "Location": "Nashville",
+            "major": "Information System"
+        }
+    ],
+    "online courses": [
+        {
+            "name": "JavaScript",
+            "school": "Udacity",
+        }
+    ]
+}
